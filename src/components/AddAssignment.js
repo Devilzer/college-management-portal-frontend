4dc03328f79ab37fdb9ad26d033922d6cf870071
Devilzer <jenadeepak724@gmail.com>
2020-12-29 19:50:17 +0530

    add assignment component

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -3,7 +3,13 @@ import React from 'react';
 function AddAssignment() {
     return (
         <div className="add-assignments">
-            
+            <input type="text"  placeholder="Title"/>
+            <textarea className="discp" placeholder="Description..."></textarea>
+            <div className="deadline">
+            <h3>Deadline.</h3>
+            <input type="date" />
+            </div>
+            <button>Add Assignment</button>
         </div>
     )
 }
